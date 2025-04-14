d345f2192c5892463e507e704bf02f7be6a8a0b8
Julianeves01 <julianvs2007@gmail.com>
2025-04-14 18:27:29 -0300

    help

--- App.js
+++ App.js
@@ -1,6 +1,5 @@
 import React from "react";
 import { NavigationContainer } from "@react-navigation/native";
-import { createStackNavigator } from "@react-navigation/stack";
 
 import Home from './pages/Home.js';
 import Detalhes from './pages/Detalhes.js';
