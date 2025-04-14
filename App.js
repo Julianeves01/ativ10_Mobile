a6930cd84c33f788eac6919034edd365b8a65c1f
Julianeves01 <julianvs2007@gmail.com>
2025-04-14 17:02:16 -0300

    update App.js

--- App.js
+++ App.js
@@ -1,7 +1,22 @@
 import React from "react";
-import Detalhes from "./pages/Detalhes";
-import Home from "./pages/Home";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+
+import Home from './pages/Home.js';
+import Detalhes from './pages/Detalhes.js';
+import Perfil from './pages/Perfil.js';
+
+
+const Stack = createStackNavigator();
 
 export default function App() {
-    return <Home />;
+    return (
+        <NavigationContainer>
+            <Stack.Navigator>
+                <Stack.Screen name="Home" component={Home} />
+                <Stack.Screen name="Detalhes" component={Detalhes} />
+                <Stack.Screen name="Perfil" component={Perfil}  />
+            </Stack.Navigator>
+        </NavigationContainer>
+    )
 }
